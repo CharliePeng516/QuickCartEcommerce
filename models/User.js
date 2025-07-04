f2281737f1e275ecdb27063fb15a6fbc70076c7b
CharliePeng <charliepeng516@gmail.com>
2025-07-05 00:00:53 +1000

    quick cart E commerce Linyi Peng

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    imageUrl: {
+        type: String,
+    },
+    cartItems: {
+        type: Object,
+        default: [],
+    },
+    Password: {
+        type: String,
+    },
+}, { minimize: false });
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
