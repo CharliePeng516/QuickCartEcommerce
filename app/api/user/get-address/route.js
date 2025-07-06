33462edfe58422836ad3b971e071e27430e50714
CharliePeng <charliepeng516@gmail.com>
2025-07-07 02:07:39 +1000

    add get address api

---
+++ app/api/user/get-address/route.js
@@ -0,0 +1,27 @@
+import connectDB from '@/config/db';
+import { getAuth } from '@clerk/nextjs/server';
+import Address from '@/models/address';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+
+    const addresses = await request.json();
+
+    await connectDB();
+
+    const newAddress = await Address.find({
+      userId,
+    });
+    return NextResponse.json({
+      success: true,
+      addresses: newAddress,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
