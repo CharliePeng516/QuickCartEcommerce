8cf1216478fdb30f46b40d1e5b82ce8d30591c3b
CharliePeng <charliepeng516@gmail.com>
2025-07-07 02:20:17 +1000

    fix the function of showing address

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,31 +1,68 @@
-import { addressDummyData } from "@/assets/assets";
-import { useAppContext } from "@/context/AppContext";
-import React, { useEffect, useState } from "react";
+import { addressDummyData } from '@/assets/assets';
+import { useAppContext } from '@/context/AppContext';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 const OrderSummary = () => {
+  const {
+    currency,
+    router,
+    getCartCount,
+    getCartAmount,
+    getToken,
+    user,
+    cartItems,
+    setCartItems,
+  } = useAppContext();
+  const [selectedAddress, setSelectedAddress] =
+    useState(null);
+  const [isDropdownOpen, setIsDropdownOpen] =
+    useState(false);
 
-  const { currency, router, getCartCount, getCartAmount } = useAppContext()
-  const [selectedAddress, setSelectedAddress] = useState(null);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const [userAddresses, setUserAddresses] = useState([]);
+  const [userAddresses, setUserAddresses] =
+    useState([]);
 
   const fetchUserAddresses = async () => {
-    setUserAddresses(addressDummyData);
-  }
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(
+        '/api/user/get-address',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (data.success) {
+        console.log(data.addresses);
+        setUserAddresses(data.addresses);
+        if (data.addresses.length > 0) {
+          setSelectedAddress(data.addresses[0]);
+        }
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const handleAddressSelect = (address) => {
     setSelectedAddress(address);
     setIsDropdownOpen(false);
   };
 
-  const createOrder = async () => {
-
-  }
+  const createOrder = async () => {};
 
   useEffect(() => {
-    fetchUserAddresses();
-  }, [])
+    if (user) {
+      fetchUserAddresses();
+    }
+  }, [user]);
 
   return (
     <div className="w-full md:w-96 bg-gray-500/5 p-5">
@@ -41,33 +78,59 @@ const OrderSummary = () => {
           <div className="relative inline-block w-full text-sm border">
             <button
               className="peer w-full text-left px-4 pr-2 py-2 bg-white text-gray-700 focus:outline-none"
-              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              onClick={() =>
+                setIsDropdownOpen(!isDropdownOpen)
+              }
             >
               <span>
                 {selectedAddress
                   ? `${selectedAddress.fullName}, ${selectedAddress.area}, ${selectedAddress.city}, ${selectedAddress.state}`
-                  : "Select Address"}
+                  : 'Select Address'}
               </span>
-              <svg className={`w-5 h-5 inline float-right transition-transform duration-200 ${isDropdownOpen ? "rotate-0" : "-rotate-90"}`}
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#6B7280"
+              <svg
+                className={`w-5 h-5 inline float-right transition-transform duration-200 ${
+                  isDropdownOpen
+                    ? 'rotate-0'
+                    : '-rotate-90'
+                }`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#6B7280"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
               </svg>
             </button>
 
             {isDropdownOpen && (
               <ul className="absolute w-full bg-white border shadow-md mt-1 z-10 py-1.5">
-                {userAddresses.map((address, index) => (
-                  <li
-                    key={index}
-                    className="px-4 py-2 hover:bg-gray-500/10 cursor-pointer"
-                    onClick={() => handleAddressSelect(address)}
-                  >
-                    {address.fullName}, {address.area}, {address.city}, {address.state}
-                  </li>
-                ))}
+                {userAddresses.map(
+                  (address, index) => (
+                    <li
+                      key={index}
+                      className="px-4 py-2 hover:bg-gray-500/10 cursor-pointer"
+                      onClick={() =>
+                        handleAddressSelect(
+                          address
+                        )
+                      }
+                    >
+                      {address.fullName},{' '}
+                      {address.area},{' '}
+                      {address.city},{' '}
+                      {address.state}
+                    </li>
+                  )
+                )}
                 <li
-                  onClick={() => router.push("/add-address")}
+                  onClick={() =>
+                    router.push('/add-address')
+                  }
                   className="px-4 py-2 hover:bg-gray-500/10 cursor-pointer text-center"
                 >
                   + Add New Address
@@ -97,25 +160,48 @@ const OrderSummary = () => {
 
         <div className="space-y-4">
           <div className="flex justify-between text-base font-medium">
-            <p className="uppercase text-gray-600">Items {getCartCount()}</p>
-            <p className="text-gray-800">{currency}{getCartAmount()}</p>
+            <p className="uppercase text-gray-600">
+              Items {getCartCount()}
+            </p>
+            <p className="text-gray-800">
+              {currency}
+              {getCartAmount()}
+            </p>
           </div>
           <div className="flex justify-between">
-            <p className="text-gray-600">Shipping Fee</p>
-            <p className="font-medium text-gray-800">Free</p>
+            <p className="text-gray-600">
+              Shipping Fee
+            </p>
+            <p className="font-medium text-gray-800">
+              Free
+            </p>
           </div>
           <div className="flex justify-between">
-            <p className="text-gray-600">Tax (2%)</p>
-            <p className="font-medium text-gray-800">{currency}{Math.floor(getCartAmount() * 0.02)}</p>
+            <p className="text-gray-600">
+              Tax (2%)
+            </p>
+            <p className="font-medium text-gray-800">
+              {currency}
+              {Math.floor(getCartAmount() * 0.02)}
+            </p>
           </div>
           <div className="flex justify-between text-lg md:text-xl font-medium border-t pt-3">
             <p>Total</p>
-            <p>{currency}{getCartAmount() + Math.floor(getCartAmount() * 0.02)}</p>
+            <p>
+              {currency}
+              {getCartAmount() +
+                Math.floor(
+                  getCartAmount() * 0.02
+                )}
+            </p>
           </div>
         </div>
       </div>
 
-      <button onClick={createOrder} className="w-full bg-orange-600 text-white py-3 mt-5 hover:bg-orange-700">
+      <button
+        onClick={createOrder}
+        className="w-full bg-orange-600 text-white py-3 mt-5 hover:bg-orange-700"
+      >
         Place Order
       </button>
     </div>
