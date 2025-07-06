cdc4bcf5d1935cb1c90cfa12858fd444fe5654af
CharliePeng <charliepeng516@gmail.com>
2025-07-07 02:38:17 +1000

    add function for createuser order

--- config/inngest.js
+++ config/inngest.js
@@ -1,61 +1,110 @@
-import { Inngest } from "inngest";
-import connectDB from "./db";
-import User from "@/models/User";
+import { Inngest } from 'inngest';
+import connectDB from './db';
+import User from '@/models/User';
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcart-next" });
+export const inngest = new Inngest({
+  id: 'quickcart-next',
+});
 
 // Inngest Function to save user data to MongoDB
 
-export const syncUserCreation = inngest.createFunction(
+export const syncUserCreation =
+  inngest.createFunction(
     {
-    id: "sync-user-creation",
+      id: 'sync-user-creation',
     },
-    { event: 'clerk/user.created'},
+    { event: 'clerk/user.created' },
     async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            _id: id,
-            name: `${first_name} ${last_name}`,
-            email: email_addresses[0].email_address,
-            imageUrl: image_url,
-        };
+      const {
+        id,
+        first_name,
+        last_name,
+        email_addresses,
+        image_url,
+      } = event.data;
+      const userData = {
+        _id: id,
+        name: `${first_name} ${last_name}`,
+        email: email_addresses[0].email_address,
+        imageUrl: image_url,
+      };
 
-        await connectDB();
-        await User.create(userData);
+      await connectDB();
+      await User.create(userData);
     }
-)
+  );
 
 // Inngest Function to update user data in Database
 
-export const syncUserUpdation = inngest.createFunction(
+export const syncUserUpdation =
+  inngest.createFunction(
     {
-        id: "sync-user-updation",
+      id: 'sync-user-updation',
     },
-    { event: 'clerk/user.updated'},
+    { event: 'clerk/user.updated' },
     async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            name: `${first_name} ${last_name}`,
-            email: email_addresses[0].email_address,
-            imageUrl: image_url,
-        };
+      const {
+        id,
+        first_name,
+        last_name,
+        email_addresses,
+        image_url,
+      } = event.data;
+      const userData = {
+        name: `${first_name} ${last_name}`,
+        email: email_addresses[0].email_address,
+        imageUrl: image_url,
+      };
 
-        await connectDB();
-        await User.findByIdAndUpdate(id, userData);
+      await connectDB();
+      await User.findByIdAndUpdate(id, userData);
     }
-)
+  );
 
 // Inngest Function to delete user data from Database
-export const syncUserDeletion = inngest.createFunction(
+export const syncUserDeletion =
+  inngest.createFunction(
     {
-        id: "sync-user-deletion",
+      id: 'sync-user-deletion',
     },
-    { event: 'clerk/user.deleted'},
+    { event: 'clerk/user.deleted' },
     async ({ event }) => {
-        const { id } = event.data;
+      const { id } = event.data;
 
-        await connectDB();
-        await User.findByIdAndDelete(id);
+      await connectDB();
+      await User.findByIdAndDelete(id);
     }
-)
+  );
+
+// Inngest Function to create user's order in database
+export const createUserOrder =
+  inngest.createFunction(
+    {
+      id: 'create-user-order',
+      batchEvents: {
+        maxSize: 25,
+        timeout: '5s',
+      },
+    },
+    { event: 'order/created' },
+    async ({ events }) => {
+      const orders = events.map((event) => {
+        return {
+          userId: event.data.userId,
+          items: event.data.items,
+          amount: event.data.amount,
+          address: event.data.address,
+          date: event.data.date,
+        };
+      });
+
+      await connectDB();
+      await Order.insertMany(orders);
+
+      return {
+        success: true,
+        processed: orders.length,
+      };
+    }
+  );
