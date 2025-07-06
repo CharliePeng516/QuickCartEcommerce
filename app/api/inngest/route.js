2cedc3041aa3af6a33f39ba12add813eef883fc7
CharliePeng <charliepeng516@gmail.com>
2025-07-07 02:37:33 +1000

    add inngest api to hold order

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,18 @@
-import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { serve } from 'inngest/next';
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+  createUserOrder,
+} from '@/config/inngest';
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    createUserOrder,
   ],
 });
