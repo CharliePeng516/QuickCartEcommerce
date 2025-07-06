323d21f70d3490a2ace4cf8e2547840d969f0216
CharliePeng <charliepeng516@gmail.com>
2025-07-07 02:37:54 +1000

    add route for create order

---
+++ app/api/order/create/route.js
@@ -0,0 +1,71 @@
+import connectDB from '@/config/db';
+import { getAuth } from '@clerk/nextjs/server';
+import Address from '@/models/address';
+import { NextResponse } from 'next/server';
+import Product from '@/models/product';
+import { inngest } from '@/config/inngest';
+import User from '@/models/user';
+
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const { address, items } =
+      await request.json();
+
+    if (!address || items.length === 0) {
+      return NextResponse.json({
+        success: false,
+        message: 'Invalid data',
+      });
+    }
+
+    // calculate price
+
+    const amount = await items.reduce(
+      async (total, item) => {
+        const product = await Product.findById(
+          item.product
+        );
+        if (!product) {
+          throw new Error('Product not found');
+        }
+        return (
+          total +
+          product.offerPrice * item.quantity
+        );
+      },
+      0
+    );
+
+    await inngest.send({
+      name: 'order/created',
+      data: {
+        userId,
+        items,
+        amount:
+          amount + Math.floor(amount * 0.02),
+        address,
+        date: Date.now(),
+      },
+    });
+
+    // connect to database
+    await connectDB();
+
+    // clear user cart
+    const user = await User.findById(userId);
+    user.cartItems = {};
+    await user.save();
+
+    return NextResponse.json({
+      success: true,
+      message: 'Order created successfully',
+    });
+  } catch (error) {
+    console.error('Error creating order:', error);
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
