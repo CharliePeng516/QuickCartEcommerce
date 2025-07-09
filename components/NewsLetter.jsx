2b8e3e88fac5e0f9af58f49c7441c273d1490067
CharliePeng <charliepeng516@gmail.com>
2025-07-09 19:59:10 +1000

    Changed Newsletter

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const NewsLetter = () => {
   return (
@@ -7,8 +7,9 @@ const NewsLetter = () => {
         Subscribe now & get 20% off
       </h1>
       <p className="md:text-base text-gray-500/80 pb-8">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+        Join our newsletter and be the first to
+        know about exclusive deals, new arrivals,
+        and special discounts.
       </p>
       <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
         <input
