7f9589458a231842fafa21570b996ae79d537ed1
Linyi (Charlie)  Peng <Charliepeng516@gmail.com>
2025-07-12 10:12:10 +1000

    Delete two console.log for test the data flow

--- app/store/features/ProductSlice.js
+++ app/store/features/ProductSlice.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
   const { data } = await axios.get('/api/product/list');
-  console.log('Fetched products:', data.products);
   return data.products;
 });
 
